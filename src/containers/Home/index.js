2cf86f6abe823d0a77d00cd0395a5f8101277515
172029 <derrick.sia@obf.ateneo.edu>
2020-03-28 12:19:46 +0800

    switching branch

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './style.css';
 import App from '../../App';
 import ImagesForCarousel from '../../components/ImagesForCarousel';
-
+import samplepic from '../../assets/pictures/samplepic.jpg'
 
 
 const Home = props => {
     return (
         <div>
-            <ImagesForCarousel url1='../../assets/pictures/samplepic.jpg'/>
+
+            {/* <ImagesForCarousel 
+            url1= {img src:'../../assets/pictures/samplepic.jpg'} 
+            url2='../../assets/pictures/samplepic.jpg' 
+            url3='../../assets/pictures/samplepic.jpg' 
+            url4='../../assets/pictures/samplepic.jpg'/> */}
+
+
             
         </div>
     )
