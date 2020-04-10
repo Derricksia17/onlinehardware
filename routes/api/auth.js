a4eba2d69fbd0fdf2e556c7c60d14c0e6cd9ccaa
172029 <derrick.sia@obf.ateneo.edu>
2020-04-10 17:36:03 +0800

    Login User POST request not working

---
+++ routes/api/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrpyt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+//@route POST api/auth
+//desc authenticate user and get token
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrpyt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return json webtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
